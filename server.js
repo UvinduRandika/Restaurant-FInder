1d9eeb9f22fc6f0edc334156c3d521b88c947f90
uvindu <randika.uvindu92@gmail.com>
2020-09-16 03:11:36 +1000

    restaurants_id modified

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const db = require("./db");
-const morgan =require("morgan");
-const app= express("");
+const morgan = require("morgan");
+const app = express("");
 
 app.use((cors()));
 app.use(express.json());
 
 app.get("/restaurants", async (req, res) => {
 
-    try{
+    try {
         const r = await db.query("SELECT * FROM restaurants");
-      //  console.log(r);
+        //  console.log(r);
         res.status(200).json({
             status: "success",
             results: r.rows.length,
@@ -21,26 +21,28 @@ app.get("/restaurants", async (req, res) => {
             },
         });
 
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
-   
+
 });
 
-app.get("/restaurants/:id", async (req, res) => {  
-    console.log(req.params.id);
+app.get("/restaurants/:id", async (req, res) => {
+    //console.log(req.params.id);
 
-    try{
-        const r = await db.query("SELECT * FROM restaurants where id =$1",[req.params.id]);
-        console.log(r);
+    try {
+        const restaurants = await db.query("SELECT * FROM restaurants where id =$1", [req.params.id]);
+        const reviews = await db.query("SELECT * FROM reviews where restaurant_id =$1", [req.params.id]);
+        
         res.status(200).json({
             status: "success",
             data: {
-                restaurants: r.rows[0]
+                restaurants: restaurants.rows[0],
+                reviews: reviews.rows
             },
         });
 
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
 
@@ -48,31 +50,12 @@ app.get("/restaurants/:id", async (req, res) => {
 });
 
 app.post("/restaurants", async (req, res) => {
- console.log("aaaaaaa");
- console.log(req.body);
-    try{
+    console.log("aaaaaaa");
+    console.log(req.body);
+    try {
         const r = await db.query(
             "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
-             [req.body.name, req.body.location, req.body.price_range]);
-        console.log(r);
-        res.status(200).json({
-            status: "success",
-            data: { 
-                restaurants: r.rows[0]
-            },
-        });
-
-    } catch(err){
-        console.log(err); 
-    }
-});
-
-app.put("/restaurants/:id", async (req, res) =>{
-  
-    try{
-        const r = await db.query(
-            "UPDATE restaurants set name= $1, location= $2, price_range= $3 where id= $4 returning *",
-             [req.body.name, req.body.location, req.body.price_range, req.params.id]);
+            [req.body.name, req.body.location, req.body.price_range]);
         console.log(r);
         res.status(200).json({
             status: "success",
@@ -81,30 +64,49 @@ app.put("/restaurants/:id", async (req, res) =>{
             },
         });
 
-    } catch(err){
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+
+    try {
+        const r = await db.query(
+            "UPDATE restaurants set name= $1, location= $2, price_range= $3 where id= $4 returning *",
+            [req.body.name, req.body.location, req.body.price_range, req.params.id]);
+        console.log(r);
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurants: r.rows[0]
+            },
+        });
+
+    } catch (err) {
         console.log(err);
     }
 });
 
 app.delete("/restaurants/:id", async (req, res) => {
-  
 
-    try{
+
+    try {
         const r = await db.query(
             "DELETE FROM restaurants where id= $1", [req.params.id]);
         console.log(r);
         res.status(204).json({
             status: "success",
-           
+
         });
 
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
 });
 
 //const P = process.env.PORT || 3001;
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server has started and listing on port port}");
 }) 
